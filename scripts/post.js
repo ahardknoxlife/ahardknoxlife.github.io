cf19713a5d4eef7137c5434da7487a84f9cd3bbb
Kerry Powell <powellkerry@ldschurch.org>
2018-01-25 20:20:12 -0700

    begin adding sharing ability

--- scripts/post.js
+++ scripts/post.js
@@ -5,6 +5,14 @@ fetch(`https://public-api.wordpress.com/rest/v1.1/sites/ahardknoxlife28994277.wo
         var postContainer = document.querySelector('.site-content'),
             htmlString = `
                 <h2 class="page-header">${data.title}</h2>
+                <div class="post__social">
+                    <a class="post__social__link" target="_blank" href="https://www.facebook.com/share.php?u=${encodeURIComponent(window.location.href)}">
+                        <img src="/images/facebook.svg" alt="Share on Facebook"/>
+                    </a>
+                    <a class="post__social__link" target="_blank" href="https://pinterest.com/pin/create/button/?url=${encodeURIComponent(window.location.href)}&media=${data.featured_image}&description=${data.title}">
+                        <img src="/images/pinterest-blue.svg" alt="Share on Pinterest"/>
+                    </a>
+                </div>
                 <date class="post__date">${new Date(data.date).toLocaleDateString()}</date>
                 <div class="post__content">
                     ${data.content}
