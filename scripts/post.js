fc260971a170df5b27bd78f1fdc4c8c0f0c186da
Kerry Powell <powellkerry@ldschurch.org>
2018-01-26 08:38:33 -0700

    try to get facebook share to work

--- scripts/post.js
+++ scripts/post.js
@@ -9,40 +9,22 @@ window.fbAsyncInit = function() {
 	});
 	FB.AppEvents.logPageView();
   };
- 
-  (function(d, s, id){
-	 var js, fjs = d.getElementsByTagName(s)[0];
-	 if (d.getElementById(id)) {return;}
-	 js = d.createElement(s); js.id = id;
-	 js.src = "//connect.facebook.net/en_US/sdk.js";
-	 fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
 
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+ 
 fetch(`https://public-api.wordpress.com/rest/v1.1/sites/ahardknoxlife28994277.wordpress.com/posts/${post_id}`).then(function(data){
     data.json().then(function(data) {
-        
-
-        FB.ui({
-            method: 'share_open_graph',
-            action_type: 'og.shares',
-            action_properties: JSON.stringify({
-                object: {
-                    'og:url': window.location.href,
-                    'og:title': data.title,
-                    'og:image': data.featured_image
-                }
-            })
-        },
-        function (response) {
-        // Action after response
-        });
-
-
         var postContainer = document.querySelector('.site-content'),
             htmlString = `
                 <h2 class="page-header">${data.title}</h2>
                 <div class="post__social">
-                    <a class="post__social__link" target="_blank" href="https://www.facebook.com/sharer.php?description=${data.title}&u=${encodeURIComponent(window.location.href)}&picture=${data.featured_image}">
+                    <a class="post__social__link" data-facebook="true" target="_blank" href="https://www.facebook.com/sharer.php?description=${data.title}&u=${encodeURIComponent(window.location.href)}&picture=${data.featured_image}">
                         <img src="/images/facebook.svg" alt="Share on Facebook"/>
                     </a>
                     <a class="post__social__link" target="_blank" href="https://pinterest.com/pin/create/button/?url=${encodeURIComponent(window.location.href)}&media=${data.featured_image}&description=${data.title}">
@@ -61,5 +43,24 @@ fetch(`https://public-api.wordpress.com/rest/v1.1/sites/ahardknoxlife28994277.wo
                 
             `;
         postContainer.innerHTML = htmlString;
+        document.querySelector('.post__social__link[data-facebook=true]').addEventListener('click', (ev)=> {
+            ev.preventDefault();
+            FB.ui({
+                method: 'share_open_graph',
+                action_type: 'og.shares',
+                action_properties: JSON.stringify({
+                    object: {
+                        'og:url': window.location.href,
+                        'og:title': data.title,
+                        'og:image': data.featured_image
+                    }
+                })
+            },
+            function (response) {
+            // Action after response
+            });
+        })
+        
     })
 })
+
